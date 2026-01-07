88c169999e348a68bca57d1af04a1ddb1fc208b1
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2026-01-08 00:39:36 +0100

    feat: sync desktop sound icon with local storage on page load

--- js/checksound.js
+++ js/checksound.js
@@ -3,13 +3,22 @@
  */
 function checkSoundState() {
     const soundIcon = document.getElementById('toggleAllSound');
+    const soundIconDesktop = document.getElementById('toggleAllSoundDesktop');
     soundFlag = loadSoundState();
     if (soundFlag) {
         soundIcon.classList.remove('toggleAllSoundDisabled');
         soundIcon.classList.add('toggleAllSoundEnabled');
+        if (soundIconDesktop) {
+            soundIconDesktop.classList.remove('toggleAllSoundDisabled');
+            soundIconDesktop.classList.add('toggleAllSoundEnabled');
+        }
     } else {
         soundIcon.classList.remove('toggleAllSoundEnabled');
         soundIcon.classList.add('toggleAllSoundDisabled');
+        if (soundIconDesktop) {
+            soundIconDesktop.classList.remove('toggleAllSoundEnabled');
+            soundIconDesktop.classList.add('toggleAllSoundDisabled');
+        }
     }
 }
 
